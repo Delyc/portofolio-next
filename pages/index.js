5e3e1e444f61bbdb8a992ba46bd8763513bd102d
Delyc <d.twizeyima@alustudent.com>
2023-02-22 23:59:48 +0200

    Feat: adding index on map

--- pages/index.js
+++ pages/index.js
@@ -19,29 +19,9 @@ export default function Home() {
   const router = useRouter()
   const [whereIWork, setWhereIWork] = useState(0);
   const [screenSize, setScreenSize] = useState(typeof window !== "undefined" && window.innerWidth);
-    const boxVariants = {
-        visible: { opacity: 1, x: 0, transition: { duration: 0.9 } },
-        hidden: { opacity: 0, x: screenSize > 640 ? (index % 2 == 0 ? 150 : -150) : 0 },
-    };
+  
 
-    useEffect(() => {
-        const setter = () => setScreenSize(window.innerWidth);
-        (() => {
-            if (window) {
-                setter()
-                window.addEventListener('resize', setter)
-            }
-        })();
-        return () => window?.removeEventListener('resize', setter)
-    }, []);
-
-    const control = useAnimation()
-    const [ref, inView] = useInView()
-
-    useEffect(() => {
-        inView ? control.start("visible") : control.start("hidden");
-    }, [control, inView]);
-
+    
   return (
     <>
 
@@ -144,46 +124,7 @@ With every line of code, I strive to make the web a better place.</p>
 
             </PageWrapper>
 
-            {fullstackProjects.map((project, index) => {
-              return(
-                <div key={index} index={index} className='flex flex-col-reverse gap-10 h-auto md:flex md:flex-row justify-between md:h-[45vh]  mt-20  overflow-x-hidden'>
-                <div className={`xl:w-2/5 space-y-4 order-2 ${index % 2 !== 0 ? 'md:order-2' : 'md:order-1'}`}>
-                    <p className="text-4xl text-white font-bold">{project.title}</p>
-                    <p className="text-white/70 text-xl leading-[30px] tracking-[1px]">{project.description}</p>
-                    <svg className="animate-bounce" width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M5 12V6C5 5.44772 5.44772 5 6 5H18C18.5523 5 19 5.44772 19 6V18C19 18.5523 18.5523 19 18 19H12M8.11111 12H12M12 12V15.8889M12 12L5 19" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                    <div className="flex flex-wrap gap-4">
-    
-                        {project.stacks.map((stack, index) => {
-                            return (
-                                <p key={index} className="text-white/50 bg-white/10 py-1 px-3 rounded">{stack.name}</p>
-                            )
-                        })}
-                    </div>
-                </div>
-                <motion.div
-                    ref={ref}
-                    variants={boxVariants}
-                    initial="hidden"
-                    animate={control}
-                    className="relative flex order-1 h-[30vh] md:h-[40vh] md:w-1/2">
-    
-                    <div className="w-1/2 h-1/2 relative">
-                        <Image layout="fill" className={`  rounded   ${index % 2 !== 0 ? 'md:order-1' : 'md:order-2'}`} src={project.image} alt="image" />
-    
-                    </div>
-    
-                    <div className="w-3/5 h-3/5 absolute top-[3rem]  md:top-[5rem] right-0 md:right-10">
-                        <Image layout="fill" className={` rounded ${index % 2 !== 0 ? 'md:order-1' : 'md:order-2'}`} src={project.image} alt="image" />
-    
-                    </div>
-    
-    
-                </motion.div>
-            </div>
-              )
-            })}
+           
 
     
 
@@ -197,10 +138,9 @@ With every line of code, I strive to make the web a better place.</p>
       <p className='text-white opacity-70 text-xl'>In my Free time, I like challenging myslf by creating diferent website's landing pages Which you can find below 🤩</p>
     </div>
      <section className='flex flex-wrap justify-center items-center  gap-10 mt-24'>
-      {projects.map((project)=>{
-        console.log(project)
+      {projects.map((project, index)=>{
         return(
-          <div className='flex flex-col gap-5  border border-gray-500 test rounded-lg w-[20rem]'>
+          <div key={index} className='flex flex-col gap-5  border border-gray-500 test rounded-lg w-[20rem]'>
             <p className='text-[#1d99b3] px-6 pt-6 font-bold text-xl'>{project.name}</p>
             <Image className='w-full h-[25vh] px-6 object-cover' src={project.image} width={100} height={100}/>
             <div className='w-full px-6 flex flex-col gap-5 pb-6'>
