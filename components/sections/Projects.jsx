d76317e586a30b4174ba491da7de9b8fe526e1fa
Delyc <d.twizeyima@alustudent.com>
2023-02-23 00:19:29 +0200

    Feat: Fixing images height

--- components/sections/Projects.jsx
+++ components/sections/Projects.jsx
@@ -5,12 +5,10 @@ import PageWrapper from "../Wrappers/PageWrapper";
 const Projects = () => {
     return (
         <PageWrapper>
-            {/* {fullstackProjects.map((project, index) => {
+            {fullstackProjects.map((project, index) => {
                 return(<ProjectsCard key={index} index={index} title={project.title} image={project.image}  stacks={project.stacks} description={project.description} />)
             })}
-             */}
-
-             <ProjectsCard />
+            
          
         </PageWrapper>
     );
