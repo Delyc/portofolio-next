4aeb1a9618e2b6af67390c69cdeb4add756736ba
Delyc <d.twizeyima@alustudent.com>
2023-02-22 23:32:48 +0200

    Fix: Fixing mobile responsiveness on work experience

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
-import {motion} from 'framer-motion'
 import { useRouter } from 'next/router'
 import Experience from '../data/Experience.json'
 const inter = Inter({ subsets: ['latin'] })
@@ -12,10 +11,37 @@ import { projects } from '../data/projects'
 import profile from '../public/assets/profile.jpg'
 import Projects from '../components/sections/Projects'
 import PageWrapper from '../components/Wrappers/PageWrapper'
+import fullstackProjects from '../data/fullStackProjects'
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 export default function Home() {
   const router = useRouter()
   const [whereIWork, setWhereIWork] = useState(0);
+  const [screenSize, setScreenSize] = useState(typeof window !== "undefined" && window.innerWidth);
+    const boxVariants = {
+        visible: { opacity: 1, x: 0, transition: { duration: 0.9 } },
+        hidden: { opacity: 0, x: screenSize > 640 ? (index % 2 == 0 ? 150 : -150) : 0 },
+    };
+
+    useEffect(() => {
+        const setter = () => setScreenSize(window.innerWidth);
+        (() => {
+            if (window) {
+                setter()
+                window.addEventListener('resize', setter)
+            }
+        })();
+        return () => window?.removeEventListener('resize', setter)
+    }, []);
+
+    const control = useAnimation()
+    const [ref, inView] = useInView()
+
+    useEffect(() => {
+        inView ? control.start("visible") : control.start("hidden");
+    }, [control, inView]);
+
   return (
     <>
 
@@ -43,7 +69,7 @@ With every line of code, I strive to make the web a better place.</p>
 
     <PageWrapper>
             <h1>name="Where I’ve Worked" number={"02"}</h1> 
-            <div className="flex  w-[15rem] relative  overflow-x-scroll  sm:hidden mb-10 text-white">
+            <div className="flex  w-[18rem] relative  overflow-x-scroll  sm:hidden mb-10 text-white">
               <div
                 className="absolute w-32 rounded-full transition-all delay-100 bg-white h-0.5 bottom-0"
                 style={{ left: `${whereIWork * 128}px` }}
@@ -53,7 +79,7 @@ With every line of code, I strive to make the web a better place.</p>
                   <button
                     key={k}
                     onClick={() => setWhereIWork(k)}
-                    className={`px-5  text-white outline-none py-2 min-w-[128px] w-32 text-[16px] transition-all delay-100 text-center ${
+                    className={`xl:px-5  text-white outline-none py-2 min-w-[128px] w-32 text-[16px] transition-all delay-100 text-center ${
                       whereIWork === k ? "text-white" : "text-secondary-100"
                     }`}
                   >
@@ -118,9 +144,52 @@ With every line of code, I strive to make the web a better place.</p>
 
             </PageWrapper>
 
+            {fullstackProjects.map((project, index) => {
+              return(
+                <div key={index} index={index} className='flex flex-col-reverse gap-10 h-auto md:flex md:flex-row justify-between md:h-[45vh]  mt-20  overflow-x-hidden'>
+                <div className={`xl:w-2/5 space-y-4 order-2 ${index % 2 !== 0 ? 'md:order-2' : 'md:order-1'}`}>
+                    <p className="text-4xl text-white font-bold">{project.title}</p>
+                    <p className="text-white/70 text-xl leading-[30px] tracking-[1px]">{project.description}</p>
+                    <svg className="animate-bounce" width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M5 12V6C5 5.44772 5.44772 5 6 5H18C18.5523 5 19 5.44772 19 6V18C19 18.5523 18.5523 19 18 19H12M8.11111 12H12M12 12V15.8889M12 12L5 19" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <div className="flex flex-wrap gap-4">
+    
+                        {project.stacks.map((stack, index) => {
+                            return (
+                                <p key={index} className="text-white/50 bg-white/10 py-1 px-3 rounded">{stack.name}</p>
+                            )
+                        })}
+                    </div>
+                </div>
+                <motion.div
+                    ref={ref}
+                    variants={boxVariants}
+                    initial="hidden"
+                    animate={control}
+                    className="relative flex order-1 h-[30vh] md:h-[40vh] md:w-1/2">
+    
+                    <div className="w-1/2 h-1/2 relative">
+                        <Image layout="fill" className={`  rounded   ${index % 2 !== 0 ? 'md:order-1' : 'md:order-2'}`} src={project.image} alt="image" />
+    
+                    </div>
+    
+                    <div className="w-3/5 h-3/5 absolute top-[3rem]  md:top-[5rem] right-0 md:right-10">
+                        <Image layout="fill" className={` rounded ${index % 2 !== 0 ? 'md:order-1' : 'md:order-2'}`} src={project.image} alt="image" />
+    
+                    </div>
+    
+    
+                </motion.div>
+            </div>
+              )
+            })}
+
     
 
     <Projects />
+
+
 
     <div className='w-full border border-gray-500 my-10'></div>
 
