9ba60bca71313c7dbdf574cd14505e1f67bc6ddb
Delyc <d.twizeyima@alustudent.com>
2023-02-24 11:41:09 +0200

    Feat: Adding projects links

--- components/sections/Projects.jsx
+++ components/sections/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
           <h1 className='text-4xl text-[#1d99b3] font-bold mt-28'>Projects I've built</h1>
 
             {fullstackProjects.map((project, index) => {
-                return(<ProjectsCard key={index} index={index} title={project.title} image={project.image}  stacks={project.stacks} description={project.description} />)
+                return(<ProjectsCard key={index} index={index} title={project.title} deployedLink={project.deployedLink} image={project.image}  stacks={project.stacks} description={project.description} />)
             })}
             
          
--- data/fullStackProjects.js
+++ data/fullStackProjects.js
@@ -2,7 +2,7 @@ const fullstackProjects = [
     {
         index: 1,
         title: "Easy Search",
-        deployedLink: "https://portofolio-delyc.vercel.app/",
+        deployedLink: "https://easy-searc.netlify.app/",
         githubLink: "https://portofolio-delyc.vercel.app/",
         description: "A convenient way for people to search for their lost documents, such as passports or IDs, by allowing them to claim their lost documents and receive email notifications when those documents are found.",
         stacks: [
@@ -18,7 +18,7 @@ const fullstackProjects = [
     {
         index: 2,
         title: "Be my voice",
-        deployedLink: "https://portofolio-delyc.vercel.app/",
+        deployedLink: "https://be-my-voice.netlify.app/",
         githubLink: "https://portofolio-delyc.vercel.app/",
         description: "An innovative platform designed to assist talented children living in camps by providing them with a means of showcasing their creativity to the wider community. ",
         stacks: [
@@ -35,7 +35,7 @@ const fullstackProjects = [
     {
         index: 3,
         title: "Quiz app",
-        deployedLink: "https://portofolio-delyc.vercel.app/",
+        deployedLink: "https://quiz-tes.netlify.app/",
         githubLink: "https://portofolio-delyc.vercel.app/",
         description: " A quiz app where users create an account and take a chosen quiz, upon submission they receive their grades and a quiz collection.",
         stacks: [
