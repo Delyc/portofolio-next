9567e6596a93e8bd2b4ad9f2d8d162ded501eb74
Delyc <d.twizeyima@alustudent.com>
2023-02-24 12:43:11 +0200

    Feat: Linking to sections

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="icon" href="../public/assets/profile.jpg" />
       <body>
         <Main />
         <NextScript />
