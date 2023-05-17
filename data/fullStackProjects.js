944f94c958f2ad55fd5da312b91d1f6957e39a20
delyc <d.twizeyima@alustudent.com>
2023-05-17 09:47:10 +0200

    Feat: UI clone section adding links

--- data/fullStackProjects.js
+++ data/fullStackProjects.js
@@ -2,8 +2,8 @@ const fullstackProjects = [
     {
         index: 1,
         title: "Easy Search",
-        deployedLink: "https://easy-searc.netlify.app/",
-        githubLink: "https://portofolio-delyc.vercel.app/",
+        deployedLink: "https://e-searc.netlify.app/",
+        githubLink: "https://github.com/Delyc/easy-searc",
         description: "A convenient way for people to search for their lost documents, such as passports or IDs, by allowing them to claim their lost documents and receive email notifications when those documents are found.",
         stacks: [
             { name: "Typescript" },
@@ -15,12 +15,27 @@ const fullstackProjects = [
 
     },
 
+    {
+        index: 1,
+        title: "Karla car rentals",
+        deployedLink: "https://karla-car-rentals.netlify.app/",
+        githubLink: "https://github.com/KARL-GROUP/karl-rental-fe",
+        description: "A convenient way for people to search for their lost documents, such as passports or IDs, by allowing them to claim their lost documents and receive email notifications when those documents are found.",
+        stacks: [
+            { name: "Typescript" },
+            { name: "NextJS" },
+            { name: "Redux" },
+            { name: "TailwindCSS" },
+        ],
+        image: "/assets/easysearch.png"
+
+    },
 
     {
         index: 2,
         title: "Be my voice",
         deployedLink: "https://be-my-voice.netlify.app/",
-        githubLink: "https://portofolio-delyc.vercel.app/",
+        githubLink: "https://github.com/Delyc/reddi-fe",
         description: "An innovative platform designed to assist talented children living in camps by providing them with a means of showcasing their creativity to the wider community. ",
         stacks: [
             { name: "Typescript" },
@@ -35,15 +50,14 @@ const fullstackProjects = [
 
     {
         index: 3,
-        title: "Quiz app",
-        deployedLink: "https://main--scintillating-puffpuff-bfb312.netlify.app/",
-        githubLink: "https://portofolio-delyc.vercel.app/",
-        description: " A quiz app where users quiz, upon submission they receive their grades and a quiz collection.",
+        title: "Quiz app API",
+        deployedLink: "",
+        githubLink: "https://github.com/Delyc/quiz-api",
+        description: " A quiz app API where users select quiz, upon submission they receive their grades and a quiz collection.",
         stacks: [
-            { name: "Typescript" },
-            { name: "NextJS" },
-            { name: "Redux" },
-            { name: "TailwindCSS" },
+            { name: "NodeJs" },
+            { name: "Express" },
+            { name: "MongoDB" },
         ],
         image: "/assets/quiz.png"
 
@@ -53,8 +67,8 @@ const fullstackProjects = [
     {
         index: 1,
         title: "TechSwine",
-        deployedLink: "https://main--dreamy-creponne-a997da.netlify.app/",
-        githubLink: "https://portofolio-delyc.vercel.app/",
+        deployedLink: "https://tech-swine.netlify.app/",
+        githubLink: "https://github.com/Delyc/tech-swine",
         description: "An E-commerce app for a piggery farm.",
         stacks: [
             { name: "Typescript" },
